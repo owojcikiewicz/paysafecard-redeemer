aa04ddc31354cdb9383fa662f2ce41d90afa1b9c
Oskar <saturdaysheroes.business@gmail.com>
2021-01-27 19:46:18 +0100

    moved testing to entry file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("test");
+import dotenv from "dotenv"; dotenv.config();
+import {PSC} from "./paysafecard";
+
+(async() => {
+    let client = await PSC.init({username: "oskanio1231", password: "!zaPxE9S955ziK&!Dt&A!qEzNsy$"}, {headless: false});
+    await client.login();
+})();
