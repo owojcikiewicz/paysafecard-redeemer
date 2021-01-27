f52138f2dd026fe0e597e9823c0c222b3624036b
Oskar <saturdaysheroes.business@gmail.com>
2021-01-28 00:05:37 +0100

    added manualLogin method to retrieve cookies (wip)

--- src/paysafecard.ts
+++ src/paysafecard.ts
@@ -1,4 +1,5 @@
 import puppeteer from "puppeteer";
+import {promises as fs} from "fs";
 
 interface Credentials {
     username: string;
@@ -30,6 +31,38 @@ class PSC {
         console.log("[PSC] Closing browser.");
         await this.browser.close();
     };
+    
+    static async manualLogin(browser: puppeteer.browser, credentials: Credentials): Promise<void> {
+        let url = "https://login.paysafecard.com/customer-auth/?client_id=mypinsPR&theme=mypins&locale=pl_PL&redirect_uri=https%3A%2F%2Fmy.paysafecard.com%2Fmypins-psc%2FtokenExchange.xhtml";
+        let page = await browser.pages().then(pages => pages[0]);
+
+        await page.goto(url);
+        await page.waitForTimeout(1000);
+
+        if (credentials) {
+            let usernameField = await page.waitForSelector("#username");
+            let passwordField = await page.waitForSelector("#password");
+            let loginButton = await page.waitForSelector("#loginButton");
+
+            await usernameField.type(credentials.username);
+            await passwordField.type(credentials.password);
+            await loginButton.click();
+            await page.waitForTimeout(2000);
+            
+            let smsButton = await page.waitForSelector("#setupFallbackButton");
+            await smsButton.click();
+            await page.waitForTimeout(2000);
+            
+            let confirmButton = await page.waitForSelector("#smsConfirmButton");
+            await page.waitForTimeout(15000);
+            await confirmButton.click();
+
+            let cookies = await page.cookies();
+            await fs.writeFile("./cookies.json", JSON.stringify(cookies, null, 2));
+
+            await browser.close();
+        };
+    };
 
     async login() {
         let url = this.LOGIN_PAGE;
@@ -52,6 +85,15 @@ class PSC {
     static async init(credentials: Credentials, options?: {}) {
         let browser = await puppeteer.launch(options ? options : {headless: false});
         let page = await browser.pages().then(pages => pages[0]);
+
+        if (browser) { // await fs.readFile("./cookies.json")
+            let cookieString = await (await fs.readFile("./cookies.json")).toString();
+            let cookies = await JSON.parse(cookieString);
+            await page.setCookie(...cookies);
+        } else {
+            console.log("[PSC] Cookies not detected, attempting manual login.");
+            await this.manualLogin(browser, credentials);
+        };
 
         return new this(browser, credentials, page);
     };
