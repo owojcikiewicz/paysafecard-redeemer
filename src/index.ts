d059f05961cc32a1e24eec6aa5374bf686d45e72
Oskar <saturdaysheroes.business@gmail.com>
2021-01-28 10:50:34 +0100

    use user data in testing

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import dotenv from "dotenv"; dotenv.config();
 import {PSC} from "./paysafecard";
 
 (async() => {
-    let client = await PSC.init({username: "oskanio1231", password: "!zaPxE9S955ziK&!Dt&A!qEzNsy$"}, {headless: false});
+    let client = await PSC.init({username: "oskanio1231", password: "!zaPxE9S955ziK&!Dt&A!qEzNsy$"}, {headless: false, userDataDir: "./user_data"});
     await client.login();
 })();
